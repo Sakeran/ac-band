5df0c32c46aad72f6d813ab5c8ed171f85a4c8d2
Alexander <Sakeran@gmail.com>
2021-04-16 11:51:40 -0400

    Add hero polaroid animation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-import "./js/modernizr";
+import anime from "animejs";
 
+import "./js/modernizr";
 import "./styles.scss";
+
+// Hero polaroid animtion
+anime({
+  targets: ".header-img img",
+  rotateY: [15, -15],
+  rotateX: [-3, 6],
+  direction: "alternate",
+  loop: true,
+  duration: 15000,
+  easing: "easeInOutQuad"
+});
